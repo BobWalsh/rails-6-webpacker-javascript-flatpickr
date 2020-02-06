c605f1d60c1f44bcdec555c4cd823b84c68e61a0
Bob Walsh <bob.walsh@47hats.com>
2020-02-06 06:42:13 -0800

    updating and run

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_04_12_133800) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.date "publish_date"
